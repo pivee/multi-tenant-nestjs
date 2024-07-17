b49e39522530a0b6f5ce08040428b41fbcac5af4
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2024-07-17 11:47:14 +0530

    Add core and batteries

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,33 @@ import { RequestIdMiddleware } from "@/middlewares/request-id.middleware";
 import { RequestLoggerMiddleware } from "@/middlewares/request-logger.middleware";
 import { TenantDatasourceMiddleware } from "@/middlewares/tenant-datasource.middleware";
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { TerminusModule } from "@nestjs/terminus";
+import * as Joi from "joi";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PrismaModule } from "./modules/prisma/prisma.module";
 
 @Module({
-  imports: [TerminusModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid("development", "production", "test", "staging")
+          .default("development"),
+        PORT: Joi.number().port().default(3000),
+        CORS_ORIGINS: Joi.string().default("*"),
+        DATABASE_URL: Joi.string(),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
+    TerminusModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
